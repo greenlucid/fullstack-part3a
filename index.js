c01bf3a1243344417a66d2bef88af49cbc81f462
Greenlucid <greenfirechaos@gmail.com>
2020-11-22 09:33:51 +0100

    Static build for heroku

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ morgan.token('body', (request, response) => (JSON.stringify(request.body)))
 
 const app = express()
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
