7d9caaf25d6854d9585cefdd1e36250447fe7ea9
Greenlucid <greenfirechaos@gmail.com>
2020-11-22 20:22:23 +0100

    FINISHED PART3

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config()
-const { request, response } = require('express')
 const express = require('express')
 var morgan = require('morgan')
 var cors = require('cors')
-morgan.token('body', (request, response) => (JSON.stringify(request.body)))
+morgan.token('body', request => (JSON.stringify(request.body)))
 
 const app = express()
 app.use(cors())
@@ -17,19 +16,28 @@ app.get('/', (request, response) => {
   response.send('What are you doing here?')
 })
 
-app.get("/info", (request, response) => {
-  const header = `<p>Phonebook has info for ${persons.length} people</p>`
-  const currentDate = `<p>${new Date()}</p>`
-  response.send(header + currentDate)
+app.get('/info', (request, response) => {
+  Person.countDocuments({})
+    .then(personsCount => {
+      const header = `<p>Phonebook has info for ${personsCount} people</p>`
+      const currentDate = `<p>${new Date()}</p>`
+      response.send(header + currentDate)
+    })
+    .catch(() => {
+      const errorMessage = `<p>Phonebook database is unavailable, so it cannot count entries.</p><p>${new Date()}</p>`
+      response.status(503).send(errorMessage)
+    })
 })
 
-app.get("/api/persons", (request, response, next) => {
-  Person.find({}).then(notes => {
-    response.json(notes)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(notes => {
+      response.json(notes)
+    })
+    .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       person
@@ -39,7 +47,7 @@ app.get("/api/persons/:id", (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response, next) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   const person = new Person({
@@ -54,27 +62,27 @@ app.post("/api/persons", (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.put("/api/persons/:id", (request, response, next) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
-  
+
   if (!body) {
     return response.status(400).json({
-      error: "Body is undefined"
+      error: 'Body is undefined'
     })
   } else if (!body.name) {
     return response.status(400).json({
-      error: "Name is empty or undefined"
+      error: 'Name is empty or undefined'
     })
   } else if (!body.number) {
     return response.status(400).json({
-      error: "Number is empty or undefined"
+      error: 'Number is empty or undefined'
     })
   }
 
   const newPerson = {
     number: body.number
   }
-  
+
   Person.findByIdAndUpdate(request.params.id, newPerson, { new: true, runValidators: true })
     .then(updatedPerson => {
       response.json(updatedPerson)
@@ -82,25 +90,23 @@ app.put("/api/persons/:id", (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-  console.log('QUE PASA DANIEEEEEEEEEL')
-  
-  response.status(404).send({ error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id'})
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).send({ error: error.message })
   }
--- models/person.js
+++ models/person.js
@@ -5,13 +5,13 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
-mongoose.connect(url, 
+mongoose.connect(url,
   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)    
+    console.log('error connecting to MongoDB:', error.message)
   })
 
 const personSchema = new mongoose.Schema({
